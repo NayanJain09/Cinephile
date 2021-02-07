a088468f79db0842c9a9082a7fac6ce96772c3d5
NayanJain09 <nayanjain2120@gmail.com>
2021-02-07 08:05:30 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic Movie_Search Website
+# Uses OMDB API for resolving search requests
+# Developed using Node.js and Express
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const request = require('request');
+const session = require('express-session')
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+app.set('view engine','ejs'); 
+app.use(express.static('public'))                                                       
+                            
+app.use(session({
+                 secret: 'bhai bhai',
+                 resave: false,
+                 saveUninitialized: true,
+                 cookie: { secure: true, maxAge: 1000*60 }
+}))
+
+
+app.get('/',(req,res)=>{
+    if(req.session.pageVisits)
+       req.session.pageVisits++;
+    else
+       req.session.pageVisits=1;
+    //console.log(req.session);   
+    res.render('homepage');
+})
+
+app.get('/result',(req,res)=>{
+    console.log(req.query.movieName);
+    const url = `http://www.omdbapi.com/?apikey=ecef8fbc&s=${req.query.movieName}`
+    request(url,function(err,response,body){
+        if(err)res.send("error");
+        if(res.statusCode===200){
+            const data = JSON.parse(body);
+            //console.log(data);
+            //res.send(data);
+            if(data.Response==='False')
+              res.render('movieNotFound')
+            else
+             res.render('result',{movieData : data});
+        }    
+        else{
+            res.send("error");
+        }
+    })
+})
+
+app.get('/result/:id',(req,res)=>{
+    const url = `http://www.omdbapi.com/?apikey=ecef8fbc&i=${req.params.id}`
+    request(url,function(err,response,body){
+        if(err)res.send("error");
+        if(res.statusCode===200){
+            const movie = JSON.parse(body);
+            //console.log(movie);
+            //res.send(data);
+            if(movie.Response==='False')
+              res.render('movieNotFound')
+            else
+             res.render('aboutMovie', {movie : movie});
+        }    
+        else{
+            res.send("error");
+        }
+    })
+})
+
+app.get('/homepage',(req,res)=>{
+    //console.log(req.query);
+    res.render('homepage');
+})
+
+
+app.get('/aboutMe',(req,res)=>{
+    //console.log(req.query);
+    res.render('aboutMe');
+})
+
+app.get('*',(req,res)=>{
+    //console.log(req.query);
+    res.render('movieNotFound');
+})
+
+app.listen(process.env.PORT,()=>{
+ console.log(`listening at port ${process.env.PORT}`);
+});
